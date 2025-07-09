75803393f02f363d1a3acc0d1acad3ae180669b0
Ashish Takalker <contact@holidayqr.com>
2025-07-09 20:15:42 +0530

    Customize Django Admin with inline Reels and QR preview

--- backend/core/admin.py
+++ backend/core/admin.py
@@ -3,34 +3,63 @@ from .models import Itinerary, Reel, ItineraryEnquiry
 from django.utils.html import format_html
 
 
+class ReelInline(admin.TabularInline):
+    model = Reel
+    extra = 1
+    readonly_fields = ('video_preview',)
+
+    def video_preview(self, obj):
+        if obj.video:
+            return format_html(
+                '<video width="200" controls><source src="{}" type="video/mp4"></video>',
+                obj.video.url
+            )
+        return "-"
+    video_preview.short_description = "Preview"
+
+
+class EnquiryInline(admin.TabularInline):
+    model = ItineraryEnquiry
+    extra = 0
+    readonly_fields = ('name', 'phone', 'message', 'created_at')
+    can_delete = False
+    show_change_link = False
+
+
 @admin.register(Itinerary)
 class ItineraryAdmin(admin.ModelAdmin):
-    list_display = ('title', 'destination', 'price', 'duration_days')
+    list_display = ('title', 'destination', 'price', 'duration_days', 'qr_preview')
     search_fields = ('title', 'destination')
-    list_filter = ('destination',)
+    list_filter = ('destination', 'duration_days')
+    ordering = ('title',)
+    readonly_fields = ('qr_preview',)
+    inlines = [ReelInline, EnquiryInline]
+
+    def qr_preview(self, obj):
+        if obj.qr_code:
+            return format_html('<img src="{}" width="100" />', obj.qr_code.url)
+        return "-"
+    qr_preview.short_description = "QR Code"
 
 
 @admin.register(Reel)
 class ReelAdmin(admin.ModelAdmin):
-    list_display = ('itinerary', 'preview_video', 'uploaded_at')
-    list_filter = ('itinerary',)
-    search_fields = ('itinerary__title',)
+    list_display = ('itinerary', 'video_tag', 'uploaded_at')
+    readonly_fields = ('video_tag',)
 
-    def preview_video(self, obj):
+    def video_tag(self, obj):
         if obj.video:
             return format_html(
-                '<video width="160" height="90" controls>'
-                '<source src="{}" type="video/mp4">'
-                'Your browser does not support the video tag.'
-                '</video>',
+                '<video width="200" controls><source src="{}" type="video/mp4"></video>',
                 obj.video.url
             )
         return "-"
-    preview_video.short_description = "Video Preview"
+    video_tag.short_description = "Preview"
 
 
 @admin.register(ItineraryEnquiry)
 class ItineraryEnquiryAdmin(admin.ModelAdmin):
     list_display = ('name', 'phone', 'itinerary', 'created_at')
-    search_fields = ('name', 'itinerary__title')
-    list_filter = ('created_at',)
+    search_fields = ('name', 'itinerary__title', 'message')
+    list_filter = ('created_at', 'itinerary')
+    readonly_fields = ('name', 'phone', 'message', 'created_at')
